bfaea1aec0ad42984e255a6ce01f0bd5fe93d819
Elliott Jones <elliottohj44@gmail.com>
2018-01-31 20:04:36 -0500

    Added GET /api/whoami method

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var useragent = require('express-useragent');
 // create instance of express for app, instantiate bodyParser and cors
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(useragent.express());
+
 // API url
-var api = '/api/whodisturbsmyslumber';
-app.get(api, (req, res) => {
-  console.log('200');
+var api = '/api/whoami';
+
+app.get(api, function (req, res, next) {
+  var lang = req.acceptsLanguages();
+  var software = "OS: " + req.useragent.os + ", Browser: " + req.useragent.browser;
+  var ipaddress = req.ip;
+
+  res.json({'IPaddress': ipaddress, 'language': lang[0], 'software': software});
 });
 
 app.listen(3000, () => {
